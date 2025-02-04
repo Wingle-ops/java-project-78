fba53cd100e221e900e1f338375576ff157d131f
Misha <tchechin.mihael@yandex.ru>
2025-02-04 15:11:27 +0300

    Start3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("java")
+    id("jacoco")
+    id("checkstyle")
+    application
 }
 
 group = "hexlet.code"
@@ -9,11 +12,28 @@ repositories {
     mavenCentral()
 }
 
+checkstyle {
+    toolVersion = "10.12.4"
+    configFile = file("config/checkstyle/checkstyle.xml")
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
+application {
+    mainClass = "hexlet.code.App"
+}
+
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+    }
 }
